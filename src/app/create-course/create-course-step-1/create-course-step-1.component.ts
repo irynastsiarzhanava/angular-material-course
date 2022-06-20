8945b3bf7b8ce1c760b65c5b64d018042fb80e9a
irynastsiarzhanava <iryna.stsiarzhanava@gmail.com>
2022-06-20 20:46:16 +0200

    feat: add a highlight for calendar date

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
 
 @Component({
@@ -21,6 +22,14 @@ export class CreateCourseStep1Component {
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   });
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    const date = cellDate.getDate();
+    if (view == 'month') {
+      return (date == 1) ? 'highlight-date' : ''
+    }
+    return '';
+  }
 
   constructor(private fb: FormBuilder) {
 
